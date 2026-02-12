29010cccd2ad013002672ecb61f796925bbf9a63
aasensi8 <aasensi8@xtec.cat>
2026-02-12 10:03:37 +0100

    Add files via upload

---
+++ elementsData.js
@@ -0,0 +1,46 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2867
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 const elements = [\
+\{Z:1, symbol:"H", name:"Hidrogen", group:1, period:1, block:"s"\},\
+\{Z:2, symbol:"He", name:"Heli", group:18, period:1, block:"s"\},\
+\{Z:3, symbol:"Li", name:"Liti", group:1, period:2, block:"s"\},\
+\{Z:4, symbol:"Be", name:"Beril\'b7li", group:2, period:2, block:"s"\},\
+\{Z:5, symbol:"B", name:"Bor", group:13, period:2, block:"p"\},\
+\{Z:6, symbol:"C", name:"Carboni", group:14, period:2, block:"p"\},\
+\{Z:7, symbol:"N", name:"Nitrogen", group:15, period:2, block:"p"\},\
+\{Z:8, symbol:"O", name:"Oxigen", group:16, period:2, block:"p"\},\
+\{Z:9, symbol:"F", name:"Fluor", group:17, period:2, block:"p"\},\
+\{Z:10, symbol:"Ne", name:"Ne\'f3", group:18, period:2, block:"p"\},\
+\{Z:11, symbol:"Na", name:"Sodi", group:1, period:3, block:"s"\},\
+\{Z:12, symbol:"Mg", name:"Magnesi", group:2, period:3, block:"s"\},\
+\{Z:13, symbol:"Al", name:"Alumini", group:13, period:3, block:"p"\},\
+\{Z:14, symbol:"Si", name:"Silici", group:14, period:3, block:"p"\},\
+\{Z:15, symbol:"P", name:"F\'f2sfor", group:15, period:3, block:"p"\},\
+\{Z:16, symbol:"S", name:"Sofre", group:16, period:3, block:"p"\},\
+\{Z:17, symbol:"Cl", name:"Clor", group:17, period:3, block:"p"\},\
+\{Z:18, symbol:"Ar", name:"Arg\'f3", group:18, period:3, block:"p"\},\
+\{Z:19, symbol:"K", name:"Potassi", group:1, period:4, block:"s"\},\
+\{Z:20, symbol:"Ca", name:"Calci", group:2, period:4, block:"s"\},\
+\{Z:21, symbol:"Sc", name:"Escandi", group:3, period:4, block:"d"\},\
+\{Z:22, symbol:"Ti", name:"Titani", group:4, period:4, block:"d"\},\
+\{Z:23, symbol:"V", name:"Vanadi", group:5, period:4, block:"d"\},\
+\{Z:24, symbol:"Cr", name:"Crom", group:6, period:4, block:"d"\},\
+\{Z:25, symbol:"Mn", name:"Mangan\'e8s", group:7, period:4, block:"d"\},\
+\{Z:26, symbol:"Fe", name:"Ferro", group:8, period:4, block:"d"\},\
+\{Z:27, symbol:"Co", name:"Cobalt", group:9, period:4, block:"d"\},\
+\{Z:28, symbol:"Ni", name:"N\'edquel", group:10, period:4, block:"d"\},\
+\{Z:29, symbol:"Cu", name:"Coure", group:11, period:4, block:"d"\},\
+\{Z:30, symbol:"Zn", name:"Zinc", group:12, period:4, block:"d"\},\
+\{Z:31, symbol:"Ga", name:"Gal\'b7li", group:13, period:4, block:"p"\},\
+\{Z:32, symbol:"Ge", name:"Germani", group:14, period:4, block:"p"\},\
+\{Z:33, symbol:"As", name:"Ars\'e8nic", group:15, period:4, block:"p"\},\
+\{Z:34, symbol:"Se", name:"Seleni", group:16, period:4, block:"p"\},\
+\{Z:35, symbol:"Br", name:"Brom", group:17, period:4, block:"p"\},\
+\{Z:36, symbol:"Kr", name:"Cript\'f3", group:18, period:4, block:"p"\}\
+];\
+}
